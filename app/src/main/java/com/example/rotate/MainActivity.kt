eed6b6f2e7f53b42905a7a33e8937cf5eb51220d
S. Kurabachew <surafel.kurabachew@hva.nl>
2021-03-16 17:32:42 +0100

    Rotation and zoom in combination with image almost done

--- app/src/main/java/com/example/rotate/MainActivity.kt
+++ app/src/main/java/com/example/rotate/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.rotate
 
 import RotationZoomGestureDetector
 import RotationZoomGestureDetector.OnRotationZoomGestureListener
+import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.scaleMatrix
 
 
 //class MainActivity : AppCompatActivity() {
@@ -16,11 +19,17 @@ import androidx.appcompat.app.AppCompatActivity
 //}
 
 class MainActivity : AppCompatActivity(), OnRotationZoomGestureListener {
+
+    private var mazeImage: ImageView? = null
+    private var mazeMatrix: Matrix = Matrix()
     private var mRotationDetector: RotationZoomGestureDetector? = null
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mRotationDetector = RotationZoomGestureDetector(this)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        mazeImage = findViewById<ImageView>(R.id.imageView)
+        mazeMatrix = mazeImage!!.getImageMatrix()
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -33,6 +42,19 @@ class MainActivity : AppCompatActivity(), OnRotationZoomGestureListener {
         val zoom = rotationZoomDetector!!.zoom
         Log.d("RotationGestureDetector", "Rotation: " + java.lang.Float.toString(angle))
         Log.d("RotationGestureDetector", "Zoom: " + java.lang.Float.toString(zoom))
+
+        val newMatrix = mazeMatrix
+
+        newMatrix.postRotate(angle, 200f, 400f)
+        newMatrix.postScale(zoom, zoom)
+
+        mazeImage?.imageMatrix = newMatrix
+
+
+//        mazeImage!!.setImageMatrix(newMatrix)
+//        postRotate(float degrees)
+//        postScale (float sx,
+//            float sy)
     }
 }
 
